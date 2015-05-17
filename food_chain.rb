69cc02475ccfc497e183ea772bddf791885da648
Katrina Owen <katrina.owen@gmail.com>
2015-05-17 11:01:40 -0700

    Coalesce 'morsel' method
    I am really not sure about the name. I also tried 'food' and 'critter',
    but 'critter' is going to be used in many ways. Both the thing that the
    lady swallowed, and the chain.
    
    Interestingly, I didn't just choose two branches and made them more
    similar, I did a horizontal refactoring, coalescing the whole idea of
    the morsel from each branch.
    
    This is partly because verses 1, 2, and 8 are all quite different, and
    it really felt weird to add an if/else with 3 and 4 in it. There is no
    'default' morsel.

--- food_chain.rb
+++ food_chain.rb
@@ -10,28 +10,28 @@ module FoodChain
   def self.verse(number)
     case number
     when 1
-      "I know an old lady who swallowed a fly.\n" +
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
       "I don't know why she swallowed the fly. Perhaps she'll die."
     when 2
-      "I know an old lady who swallowed a spider.\n" +
-      "It wriggled and jiggled and tickled inside her.\n" +
-      "She swallowed the spider to catch the fly.\n" +
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
+      "It wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
       "I don't know why she swallowed the fly. Perhaps she'll die."
     when 3
-      "I know an old lady who swallowed a bird.\n" +
-      "How absurd to swallow a bird!\n" +
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-      "She swallowed the spider to catch the fly.\n" +
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
+      "How absurd to swallow a bird!\n" \
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
       "I don't know why she swallowed the fly. Perhaps she'll die."
     when 4
-      "I know an old lady who swallowed a cat.\n" +
-      "Imagine that, to swallow a cat!\n" +
-      "She swallowed the cat to catch the bird.\n" +
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-      "She swallowed the spider to catch the fly.\n" +
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
+      "Imagine that, to swallow a cat!\n" \
+      "She swallowed the cat to catch the bird.\n" \
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
       "I don't know why she swallowed the fly. Perhaps she'll die."
     when 5
-      "I know an old lady who swallowed a dog.\n" \
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
       "What a hog, to swallow a dog!\n" \
       "She swallowed the dog to catch the cat.\n" \
       "She swallowed the cat to catch the bird.\n" \
@@ -39,7 +39,7 @@ module FoodChain
       "She swallowed the spider to catch the fly.\n" \
       "I don't know why she swallowed the fly. Perhaps she'll die."
     when 6
-      "I know an old lady who swallowed a goat.\n" \
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
       "Just opened her throat and swallowed a goat!\n" \
       "She swallowed the goat to catch the dog.\n" \
       "She swallowed the dog to catch the cat.\n" \
@@ -48,7 +48,7 @@ module FoodChain
       "She swallowed the spider to catch the fly.\n" \
       "I don't know why she swallowed the fly. Perhaps she'll die."
     when 7
-      "I know an old lady who swallowed a cow.\n" \
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
       "I don't know how she swallowed a cow!\n" \
       "She swallowed the cow to catch the goat.\n" \
       "She swallowed the goat to catch the dog.\n" \
@@ -58,8 +58,21 @@ module FoodChain
       "She swallowed the spider to catch the fly.\n" \
       "I don't know why she swallowed the fly. Perhaps she'll die."
     when 8
-      "I know an old lady who swallowed a horse.\n" \
+      "I know an old lady who swallowed #{morsel(number)}.\n" \
       "She's dead, of course!"
     end
+  end
+
+  def self.morsel(i)
+    [
+      "a fly",
+      "a spider",
+      "a bird",
+      "a cat",
+      "a dog",
+      "a goat",
+      "a cow",
+      "a horse",
+    ][i-1]
   end
 end
